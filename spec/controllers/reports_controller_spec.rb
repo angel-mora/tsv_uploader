80bde0cffcb79e5796b37f12988096e16c65c949
AngelMoraDev <angelmomaa@gmail.com>
2021-07-14 14:50:20 -0500

    fix rspec

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -1,6 +1,6 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe ReportsController do
+RSpec.describe ReportsController, type: :controller do
   describe 'POST #import' do
     it 'redirects to the home page' do
       allow(User).to receive(:import).with('foo.txt')
